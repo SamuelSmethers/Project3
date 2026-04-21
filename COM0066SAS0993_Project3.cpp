e22d75d689145be90e136385f435624dbb98ecb2
SamuelSmethers <ssmethers16@yahoo.com>
2026-04-21 04:27:55 +0000

    updates

--- COM0066SAS0993_Project3.cpp
+++ COM0066SAS0993_Project3.cpp
@@ -407,6 +407,15 @@ void withdraw(double& balance, AccountType type, string& pin, bool& pinSet)
 
 }
 
+// Hash PIN
+unsigned long hashPin(const string& pin) {
+    unsigned long h = 0;
+    for (int i = 0; i < (int)pin.size(); i++) {
+        h = h * 31 + pin.at(i);
+    }
+    return h;
+}
+
 void saveToFile(string file_name)
 {
 	o_f.open(file_name);
